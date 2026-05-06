9d2a7a3a636d7cf608c3cc4c09cb8e2bfbf79a02
Prism Dev <dev@prism.app>
2026-05-06 22:41:27 +0800

    fix: remove Flipper from MainApplication.kt to fix release build

--- android/app/src/main/java/com/prism/MainApplication.kt
+++ android/app/src/main/java/com/prism/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -35,6 +34,5 @@ class MainApplication : Application(), ReactApplication {
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             load()
         }
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
 }
